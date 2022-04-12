1e321d633231c1641bca4a74f52a1c60200dec97
Adam <44130552+adsalad@users.noreply.github.com>
2022-04-12 16:00:58 -0400

    diffable collection view implementation to display followers

---
+++ GithubFollowers/Custom Views/Cells/GFFollowerCell.swift
@@ -0,0 +1,49 @@
+//
+//  GFFollowerCell.swift
+//  GithubFollowers
+//
+//  Created by Adam S on 2022-04-10.
+//
+
+import UIKit
+
+class GFFollowerCell: UICollectionViewCell {
+    
+    static let reuseID = "GFFollowerCell"
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func set(follower: Follower){
+        usernameLabel.text = follower.login
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor), //^
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 8),
+        ])
+    }
+    
+}
--- GithubFollowers/Screens/FollowersListVC.swift
+++ GithubFollowers/Screens/FollowersListVC.swift
@@ -9,24 +9,84 @@ import UIKit
 
 class FollowersListVC: UIViewController {
     
-    var username : String!
+    enum Section {
+        case main
+    }
+    
+    var username: String!
+    var followers: [Follower] = []
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionVIew()
+        configureDataSource()
+        getFollowers()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func configureCollectionVIew(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: configureThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(GFFollowerCell.self, forCellWithReuseIdentifier: GFFollowerCell.reuseID)
+    }
+    
+    func configureThreeColumnFlowLayout() -> UICollectionViewFlowLayout  {
+        let width                       = view.bounds.width
+        let padding : CGFloat           = 12
+        let minItemSpacing : CGFloat    = 8
+        let availableWidth              = width - (padding * 2) - (minItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GFFollowerCell.reuseID, for: indexPath) as! GFFollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             switch result {
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        navigationController?.isNavigationBarHidden = false
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        //diffables can be called from background thread
+        DispatchQueue.main.sync {
+            dataSource.apply(snapshot, animatingDifferences: true)
+        }
     }
 }
