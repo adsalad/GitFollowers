977aa103212a75b04de551c2f33f2abf0a39ac1e
Adam <44130552+adsalad@users.noreply.github.com>
2022-04-13 23:59:40 -0400

    Image Caching

--- GithubFollowers/Custom Views/GFAvatarImageView.swift
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class GFAvatarImageView: UIImageView {
-
-    let placeholderImage = UIImage(named: "avatar-placeholder")!
-
+    
+    let cache               = NetworkManager.shared.cache
+    let placeholderImage    = UIImage(named: "avatar-placeholder")!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -26,6 +27,31 @@ class GFAvatarImageView: UIImageView {
         image               = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
         
+    }
+    
+    // didnt see the point of putting this in NetworkManager since we don't need error handling for image retreival
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = image
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            self?.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+        task.resume()
     }
     
 }
--- GithubFollowers/Model/NetworkManager.swift
+++ GithubFollowers/Model/NetworkManager.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkManager {
-    static let shared   = NetworkManager()
-    let baseURL         = "https://api.github.com"
+    static let shared           = NetworkManager()
+    private let baseURL         = "https://api.github.com"
+    let cache                   = NSCache<NSString, UIImage>()
         
     private init() {}
     
