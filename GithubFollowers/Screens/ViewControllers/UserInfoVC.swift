1f6cb8ff5067a984cb8a7bf1ef67a75a0570c344
Adam <44130552+adsalad@users.noreply.github.com>
2022-04-22 18:46:22 -0400

    Cleanup + Preparing VC for InfoViews

--- GithubFollowers/Screens/ViewControllers/UserInfoVC.swift
+++ GithubFollowers/Screens/ViewControllers/UserInfoVC.swift
@@ -10,17 +10,26 @@ import UIKit
 class UserInfoVC: UIViewController {
     
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    var itemViews: [UIView] = []
     
     var username: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        getUserInfo()
+        layoutUI()
+    }
+    
+    func configureViewController(){
         view.backgroundColor                = .systemBackground
         let doneButton                      = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem   = doneButton
-        
-        layoutUI()
-        
+    }
+    
+    func getUserInfo() {
         NetworkManager.shared.getUser(for: username) {[weak self] result in
             
             guard let self = self else { return }
@@ -34,21 +43,36 @@ class UserInfoVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Something went wrong!", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-        
     }
     
     func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // adds leading and trailing constraints so that views dont bleed to left/right edge of screen
+            // using a loop to simplify the code, otherwise the below constraint array would have very repetitive code
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            ])
+        }
+        
+        itemViewTwo.backgroundColor = .systemRed
+        itemViewOne.backgroundColor = .systemRed
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 20),
+            itemViewOne.heightAnchor.constraint(equalToConstant: 140),
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: 20),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: 140),
         ])
     }
-    
     
     func add(childVC: UIViewController, to containerView: UIView) {
         addChild(childVC)
